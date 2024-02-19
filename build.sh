72eebc621ed8571f8903f03614edfe35549687f8
infiniteregrets <aroram18@mcmaster.ca>
2024-02-18 22:28:02 -0500

    Update

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ if [ "$HUGO_ENVIRONMENT" == "production" ]; then
     npm run build;    
 else
     npm run build -- -b $CF_PAGES_URL;
+    npm install pagefind;
+    npx pagefind --site public/docs --output-path static/pagefind
 fi;
-npx pagefind --site public/docs --output-path static/pagefind
