656e2475a225cc51d04938047488df7f0943772b
Aviram Hassan <aviramyhassan@gmail.com>
2024-02-20 12:52:58 +0200

    run pagefind after npm build

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-npm run build;
-npx pagefind --site public/docs --output-path static/pagefind
 if [ "$HUGO_ENVIRONMENT" == "production" ]; then
     npm run build;
 else
     npm run build -- -b $CF_PAGES_URL;
 fi;
+
+npx pagefind --site public/docs --output-path static/pagefind
