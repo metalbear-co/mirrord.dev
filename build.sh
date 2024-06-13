d8d7498336e198fb2b7f394e5c07f2c807ab85c3
Eyal Bukchin <eyal@metalbear.co>
2024-06-13 10:50:27 +0300

    Revert "New website"
    This reverts commit 0cd5d6367839f848ebf16874d87329e794a86f04.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$HUGO_ENVIRONMENT" == "production" ]; then
+    npm run build;
+else
+    npm run build -- -b $CF_PAGES_URL;
+fi;
+
+npx pagefind --site public/docs --output-path public/pagefind
